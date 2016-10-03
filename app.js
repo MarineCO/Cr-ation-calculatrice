641ac766a3098cd3f311f3e970de9fa948542f8d
marineco <marinecolonge@gmail.com>
2016-10-03 22:55:28 +0200

    Ajout version 2 calculatrice Pbm d'affichage du résultat dans l'input, ne fonctionne qu'en console.log pr le moment

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 // CALCULATRICE
 
+/* METHODE 1 INPUT */
+
 (function(){
 	// console.log(Bienvenue);
 
@@ -20,7 +22,7 @@
 		var resultat = calculer(element1, element2, operation);
 		// console.log('cal' + operation);
 
-		$('#result').text(resultat);
+		$('#result').html(resultat);
 	}
 
 	function add(x, y){
@@ -60,8 +62,118 @@
 
 		} else if (operation === '/'){
 			return divide(nb1, nb2);
+		} else {
+			return alert('Il y a une erreur, recommencez')
 		}
 
 	}
 
+})();
+
+
+
+
+/* METHODE 2 BUTTON */
+
+(function(){
+
+	console.log('methode2');
+
+// Point de départ : variables non déterminés
+
+var nombre1;
+var nombre2;
+var opeRation; 
+var result = 0;
+
+
+// Définition des différentes opérations utiles
+
+function add(x, y){
+	return x + y;
+}
+
+function substract(x, y){
+	return x - y;
+}
+
+function multiply(x, y){
+	return x * y;
+}
+
+function divide(x, y){
+	return x / y;
+}
+
+function calculer(x, y, opeRation) {
+
+	if (opeRation === '+'){
+		return add(x, y);
+
+	} else if (opeRation === '-'){
+		return substract(x, y);
+
+	}else if (opeRation === '*'){
+		return multiply(x, y);
+
+	} else if (opeRation === '/'){
+		return divide(x, y);
+	} else {
+		return alert('Il y a une erreur, recommencez')
+	}
+}
+
+// Click sur les nombres 
+
+$(".number").click(function(){
+	var nombre = parseInt($(this).data('number'));
+	console.log(nombre);
+
+	if (nombre1 === undefined) {
+		nombre1 = nombre;
+		$("#resultat").text(nombre);
+
+	} else {
+		nombre2 = nombre;
+		$("#resultat").text(nombre);
+	}
+
+});
+
+// Click sur les opérateurs 
+
+$(".ope").click(function(){
+
+	opeRation = $(this).data('operateur');
+	console.log(opeRation);
+
+	if (opeRation === null){
+		opeRation = 'add';
+		$("#resultat").text('+');
+
+
+	} else if (opeRation === null){
+		opeRation = 'substract';
+		$("#resultat").text('-');
+
+	} else if (opeRation === null){
+		opeRation = 'multiply';
+		$("#resultat").text('*');
+
+	} else if (opeRation === null){
+		opeRation = 'divide';
+		$("#resultat").text('/');
+	}
+});
+
+// Click sur le = 
+
+$(".egal").click(function() {
+
+	result = calculer(nombre1, nombre2, opeRation);
+	console.log(result);
+
+	$("#resultat").text(result);
+
+});
 })();
